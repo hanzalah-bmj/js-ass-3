f9f9bd29c16438846b4d665673ef0df0b2b6a724
hanzalah-bmj <hanzalah.bmj@gmail.com>
2023-10-18 15:27:43 +0500

    JS Ass 3

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(){
+    let numO = parseInt(document.getElementById("num1").value);
+    let numT = parseInt(document.getElementById("num2").value);
+    result = numO+numT;
+    document.getElementById("heading").innerText = "Addition of " + numO + " & " + numT + " is " + result;
+}
+function sub(){
+    let numO = parseInt(document.getElementById("num1").value);
+    let numT = parseInt(document.getElementById("num2").value);
+    result = numO-numT;
+    document.getElementById("heading").innerText = "Subtraction " + numO + " & " + numT + " is " + result;
+}
+function mul(){
+    let numO = parseInt(document.getElementById("num1").value);
+    let numT = parseInt(document.getElementById("num2").value);
+    result = numO*numT;
+    document.getElementById("heading").innerText = "Multiplication of " + numO + " & " + numT + " is " + result;
+}
+function div(){
+    let numO = parseInt(document.getElementById("num1").value);
+    let numT = parseInt(document.getElementById("num2").value);
+    result = numO/numT;
+    document.getElementById("heading").innerText = "Division of " + numO + " & " + numT + " is " + result;
+}
+
